b3951e2ff8c76e473bec63f5a3b4399e1b9de911
NP2 <sahandraeisi1994@gmail.com>
2019-07-15 10:30:32 +0430

    Added Custom TableView cell

---
+++ OperationQueue/View/Cell/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  OperationQueue
+//
+//  Created by NP2 on 7/15/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+final class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
