6b13b6bcebf8ab2560296cb868bdaffd65fdb523
NP2 <sahandraeisi1994@gmail.com>
2019-07-15 10:07:57 +0430

    Initial Commit

---
+++ OperationQueue/ListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ListViewController.swift
+//  OperationQueue
+//
+//  Created by NP2 on 7/15/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+final class ListViewController: UIViewController {
+    
+    
+    
+    
+    
+}
+
+// MARK: - Life Cycle
+
+extension ListViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: TableView DataSource Delegate
---
+++ OperationQueue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OperationQueue
+//
+//  Created by NP2 on 7/15/19.
+//  Copyright © 2019 shndrs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
